ae335e98061fb7d3e87c2eb4396a6edfed17a768
ALadysz <s4204218@glos.ac.uk>
2024-01-28 02:09:10 +0000

    File Cleanup

--- Assets/MainScripts/Unused/OneBigLine.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class OneBigLine : MonoBehaviour
-{
-    //make new line every time and make it to nearest star OR from first generated star make line to closest and then so on
-}
